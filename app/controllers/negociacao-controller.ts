6810f9950f820f7e6034b323772b3cc3d9057744
edio <dev.edioj@gmail.com>
2022-02-10 23:13:51 -0300

    final

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,10 +1,11 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
-
+    private negociacoes = new Negociacoes();
     constructor() {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
@@ -13,8 +14,9 @@ export class NegociacaoController {
 
     adiciona(): void {
         
-        const negociacao = this.criaNegociacao;
-        console.log(negociacao)
+        const negociacao = this.criaNegociacao();
+        this.negociacoes.adiciona(negociacao);
+        console.log(this.negociacoes.lista());
         this.limparFormulario();
     }
 
